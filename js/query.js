4323bead9b6a19659dde56403c307e0b7562fba9
Eimantas Rebždys <1069319@activedir.vu.lt>
2019-09-12 18:33:17 +0300

    improvements

--- js/query.js
+++ js/query.js
@@ -1,125 +1,132 @@
 function CheckId(number) {
     if((number.length == 7) || (number.length == 9)) {//Juridinis asmuo
-        var juridioAsmensStandartas = "";
-        var kontrolineSuma = "";
-
+        info = {"type":"J"};
         if(number.length == 7) {
-            juridioAsmensStandartas = "Senas juridinio asmens kodo standartas.";
+            info.standart = "Senas juridinio asmens kodo standartas.";
         } else if(number.length == 9) {
-            juridioAsmensStandartas = "Naujasis juridinio asmens kodo standartas.";
-
+            info.standart = "Naujasis juridinio asmens kodo standartas.";
             var s = Number(number[0])*1 + Number(number[1])*2 + Number(number[2])*3;
             s += Number(number[3])*4 + Number(number[4])*5 + Number(number[5])*6 + Number(number[6])*7;
             s += Number(number[7])*8;
-            var controlNum = s % 11;
-            if(controlNum == 10) {
+            var checksum = s % 11;
+            if(checksum == 10) {
                 return "Error";
             }
-            if(controlNum == Number(number[8])) {
-                kontrolineSuma = "Juridinio asmens kodo kontrolinė suma sutampa."
+            else if(checksum == Number(number[8])) {
+                info.checksum = "Juridinio asmens kodo kontrolinė suma sutampa."
             } else {
-                kontrolineSuma = "KLAIDA: Juridinio asmens kodo kontrolinė suma nesutampa.";
-                //kontrolineSuma += "\n(Turėtų būti: " + controlNum + " Yra: " + Number(number[8]) + ")";
+                info.checksum = "KLAIDA: Juridinio asmens kodo kontrolinė suma nesutampa.";
             }
         }
-        return juridioAsmensStandartas + "\n" + kontrolineSuma;
+        return info
     } else if(number.length == 11) { //Fizinis asmuo
-        var lytis = "";
-        var gimimoMetai = "";
-        var gimimoMenuo = "";
-        var gimimoDiena = "";
-        var asmensKodoStandartas = "";
-        var kontrolineSuma = "";
-
+        var checksum = "";
         if(Number(number[0]) >= 1 && Number(number[0]) <= 6) {
-            if(number[0] == "1" || number[0] == "2") {
-                gimimoMetai = "18";
-            } else if(number[0] == "3" || number[0] == "4") {
-                gimimoMetai = "19";
-            } else if(number[0] == "5" || number[0] == "6") {
-                gimimoMetai = "20";
-            }
-            gimimoMetai += number.substring(1, 3);
-            gimimoMenuo = number.substring(3, 5);
-            gimimoDiena = number.substring(5, 7);
-
-            if(Number(gimimoMenuo) < 1 || Number(gimimoMenuo) > 12) {
-                return "KLAIDA: Asmens kode įvestas neteisingas gimimo mėnuo.";
-            }
-            if(Number(gimimoDiena) < 1 || Number(gimimoDiena) > 31) {
+            var info = calculateBirthInfo(number);
+            if(Number(info.month) < 1 || Number(info.month) > 12) {
+                return "KLAIDA: Asmens kode įvestas neteisingas gimimo mėnuo."; //menesis neatitinka
+            }if(Number(info.day) < 1 || Number(info.day) > 31) {
                 return "KLAIDA: Asmens kode įvestas neteisinga gimimo diena.";
             }
-            asmensKodoStandartas = "Standartinis fizinio asmens kodas.";
-
-            var s = Number(number[0])*1 + Number(number[1])*2 + Number(number[2])*3;
-            s += Number(number[3])*4 + Number(number[4])*5 + Number(number[5])*6 + Number(number[6])*7;
-            s += Number(number[7])*8 + Number(number[8])*9 + Number(number[9])*1;
-            var controlNum = s % 11;
-            if(controlNum == 10) {
-                s = Number(number[0])*3 + Number(number[1])*4 + Number(number[2])*5;
-                s += Number(number[3])*6 + Number(number[4])*7 + Number(number[5])*8;
-                s += Number(number[6])*9 + Number(number[7])*1 + Number(number[8])*2 + Number(number[9])*3;
-                controlNum = s % 11;
-                if(controlNum == 10) {
-                    controlNum = 0;
-                }
-            }
-
-            if(controlNum == Number(number[10])) {
-                kontrolineSuma = "Asmens kodo kontrolinė suma sutampa."
-            } else {
-                kontrolineSuma = "KLAIDA: Asmens kodo kontrolinė suma netampa.";
-                //kontrolineSuma += "\n(Turėtų būti: " + controlNum + " Yra: " + Number(number[10]) + ")";
-            }
+           info.checksum = validateId(number);
         } else if(number[0] == "9") {
-            asmensKodoStandartas = "Nestandartinis fizinio asmens kodas.";
+            info={};
+            info.sex="Nežinoma";
+            info.year="Nežinoma";
+            info.day="";
+            info.month="";
+            return info;
         } else {
-            gimimoMetai = "KLAIDA: Nenustatytas";
+            info.year = "KLAIDA: Nenustatytas";
         }
-
-
         if(number[0] == "1" || number[0] == "3" || number[0] == "5") {
-            lytis = "Vyras";
+            info.sex = "Vyras";
         } else if(number[0] == "2" || number[0] == "4" || number[0] == "6") {
-            lytis = "Moteris";
+            info.sex = "Moteris";
         }
-
-        return lytis + " --- " + gimimoMetai + "-" + gimimoMenuo + "-" + gimimoDiena + "\n" + asmensKodoStandartas + "\n" + kontrolineSuma;
+        return info;
     } else {
         return "KLAIDA: Skaičius nėra nei juridinio nei fizinio asmens kodas";
     }
 }
 
+function calculateBirthInfo(number){
+    var info = {};
+    if(Number(number[0]) >= 1 && Number(number[0]) <= 6) {           
+        if(number[0] == "1" || number[0] == "2") {
+            info.year = "18"
+        } else if(number[0] == "3" || number[0] == "4") {
+            info.year = "19"
+        } else if(number[0] == "5" || number[0] == "6") {
+            info.year = "20"
+        }
+        info.year += number.substring(1, 3);
+        info.month = number.substring(3, 5);
+        info.day = number.substring(5, 7);
+        return info;
+    }
+}
+
+function validateId(number){
+    var s = Number(number[0])*1 + Number(number[1])*2 + Number(number[2])*3;
+    s += Number(number[3])*4 + Number(number[4])*5 + Number(number[5])*6 + Number(number[6])*7;
+    s += Number(number[7])*8 + Number(number[8])*9 + Number(number[9])*1;
+    var checksum = s % 11;
+    if(checksum == 10) {
+        s = Number(number[0])*3 + Number(number[1])*4 + Number(number[2])*5;
+        s += Number(number[3])*6 + Number(number[4])*7 + Number(number[5])*8;
+        s += Number(number[6])*9 + Number(number[7])*1 + Number(number[8])*2 + Number(number[9])*3;
+        checksum = s % 11;
+        if(checksum == 10) {
+            checksum = 0;
+        }
+    } if(checksum == Number(number[10])) {
+        return checksum;
+    } else {
+        return "ID Netinkamas";
+    }
+}
 
 function createElement(data){
 var target = $('#container');
     if($(target).children().html()==null){
-        if(true ){ //Check if result valid#
+        if(typeof data ==="object"){ //Check if result valid#
             $(target).append($('<div>',{class:'mx-auto p-4 output-area'}));
             $(target).children().append($('<div>', {class: 'mx-auto p-1 image-area',style:"color:rgba(183,183,183,0.5)"}));
-            $(target).children().first().append($('<div>'+data+'</div>'));
-            target = $(target).children().children().first();
-            $(target).append($('<i>', {class: 'fa fa-male',style:"font-size:10vh"})); 
-       
+            if(data.type==="J"){
+                $(target).children().first().append($('<div class="p-3">'+data.standart+'</div>'));
+                $(target).children().first().append($('<div class="p-3">'+data.checksum+'</div>'));
+                target = $(target).children().children().first();
+                $(target).append($('<i>', {class: 'fa fa-briefcase',style:"font-size:10vh"}));
+                return ;
+            }else{
+                $(target).children().first().append($('<div class="p-3">'+data.sex+'</div>'));
+                $(target).children().first().append($('<div class="bct">'+data.year+'-'+data.month+"-"+data.day+'</div>'));
+                if(data.checksum!="ID Netinkamas"){
+                    $(target).children().first().append($('<div class="text-success pt-3">ID Tinkamas</div>'));
+                }else{
+                    $(target).children().first().append($('<div class="text-danger pt-3 ">ID Neinkamas</div>'));
+                }
+                target = $(target).children().children().first();
+                if(data.sex == "Vyras"){
+                $(target).append($('<i>', {class: 'fa fa-male',style:"font-size:10vh"}));
+                }else if(data.sex == "Moteris"){
+                    $(target).append($('<i>', {class: 'fa fa-female',style:"font-size:10vh"}));
+                }else{
+                    $(target).append($('<i>', {class: 'fa fa-question',style:"font-size:10vh"}));
+                }
+            }  
         } else{
-            $(target).html('');
-            $(targer).append(data);
+            $(target).children().append($('<div>'+data+'</div>'));
+            //$(targer).append(data);
         }
     }else{
-     $(target).html('');
-     //continue;   
-    }
-
-    //$(target).append($('<div>', {class: 'mx-auto p-1 image-area',style:"color:rgba(183,183,183,0.5)"}));
-   // $(target).append($('<div>'+data+'</div>'));
-    //$(target).append($('<div>', {class: 'bct mt-2'}));
-    //$(target).append($('<div>', {class: 'bct mt-2'}));
-    //$(target).append($('<div>', {class: 'bct mt-2'}));
-    //target = $(target).children().first();
-   // $(target).append($('<i>', {class: 'fa fa-male',style:"font-size:10vh"}));  
+     $(target).html('');   
+     return createElement(data);
+    } 
 }
 
-function test(){
+function check(){
     var id = $("#idplaceholder").val();
     var data = CheckId(id);
     createElement(data);
